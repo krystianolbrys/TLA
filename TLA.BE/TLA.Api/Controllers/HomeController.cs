4738780332061074c1538fe90df63a0b7d6d796e
Krystian Olbrys <kolbrys@investcloud.com>
2022-01-05 20:48:34 +0100

    Structure

---
+++ TLA.BE/TLA.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLA.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return Ok(new { Status = "OK - Working" });
+        }
+    }
+}
