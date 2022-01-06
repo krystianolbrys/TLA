fb3f6f48bea34f56623c4daacb57fd7daeb3459d
Krystian Olbrys <kolbrys@investcloud.com>
2022-01-06 21:42:53 +0100

    First migration

---
+++ TLA.BE/TLA.Persistence/Migrations/20220106204026_TransaltionDbInitial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TLA.Persistence.Migrations
+{
+    public partial class TransaltionDbInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    InputWord = table.Column<string>(type: "TEXT", nullable: false),
+                    OutputWord = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
